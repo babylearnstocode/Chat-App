36ef548ac42f1c8f108b899c26d88aa32bffc35c
Huy Anh <huyanh2504@gmail.com>
2020-09-06 13:48:45 +0200

    redirect after verifying

--- routes/viewRouter.js
+++ routes/viewRouter.js
@@ -8,7 +8,7 @@ const router = express.Router();
 router.use(authController.isLoggedIn);
 router.get('/', viewController.getSignup);
 router.get('/signup', viewController.getSignup);
-// router.patch('/verifySignup/:token', authController.verifySignup);
+router.patch('/verifySignup/:token', viewController.getMessages);
 router.get('/login', viewController.getLogin);
 
 router.get(
@@ -17,40 +17,5 @@ router.get(
   authController.protect,
   viewController.getMessages
 );
-
-// router.post(
-//   '/forgotPassword',
-//   authController.forgotPassword
-// );
-// router.patch(
-//   '/resetPassword/:token',
-//   authController.resetPassword
-// );
-
-//Protect all routes after this middleware
-// router.use(authController.protect);
-
-// router.patch('/updatePassword', authController.updatePassword);
-
-// router.patch('/updateMe', userController.updateMe);
-
-// router.delete('/deleteMe', userController.deleteMe);
-
-// router.get('/me', userController.getMe, userController.getUser);
-
-// //Only admins have permission to perform these actions
-
-// router.use(authController.restrictTo('admin'));
-
-// router
-//   .route('/')
-//   .get(userController.getAllUsers)
-//   .post(userController.createUser);
-
-// router
-//   .route('/:id')
-//   .get(userController.getUser)
-//   .patch(userController.updateUser)
-//   .delete(userController.deleteUser);
 
 module.exports = router;
